e25794bf57b72022009240d0e735ff0c9c9fc6dd
Camilo Castrillon <Camiloc306766@gmail.com>
2026-04-01 09:08:11 -0500

    Merge feature--Exraction-class into integrations-main (resolviendo conflictos)

--- mongodb-docker/init.js
+++ mongodb-docker/init.js
@@ -1,9 +1,6 @@
 db = db.getSiblingDB("ETL_AIRBNB");
 
-<<<<<<< HEAD
 // crear usuario
-=======
->>>>>>> feature--Exraction-class
 db.createUser({
   user: "AIRBNB",
   pwd: "12345",
@@ -13,13 +10,9 @@ db.createUser({
       db: "ETL_AIRBNB"
     }
   ]
-<<<<<<< HEAD
 });
 
 // crear colecciones
 db.createCollection("calendar");
 db.createCollection("listings");
 db.createCollection("reviews");
-=======
-});
->>>>>>> feature--Exraction-class
